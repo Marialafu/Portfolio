d272bf65d4c157b4eab57ec2596ed2bbd13c605e
Marialafu <lafuentelhmaria@gmail.com>
2025-12-16 14:06:40 +0100

    updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import './Home.css';
 import Hero from '../../components/hero/Hero';
 import Navbar from '../../components/navbar/Navbar';
-import TeoricoInHome from '../../components/homeprojects/teoricoInHome/TeoricoInHome';
 import Footer from '../../components/footer/Footer';
+import TeoricoInHome from '../../components/homePorjects/teoricoInHome/TeoricoInHome';
 
 const Home = () => {
   return (
